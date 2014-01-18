336718d732cea4ee9060e7e967bd8bd5e963f0ea
Maaz Ali <m@maazali.me>
2014-01-17 23:27:13 -0500

    Incomplete Install file and default config file
    A default configuration file that holds the structure of a normal
    configuration file. Includes details of the database. The install file
    will be used to setup the auth system.

---
+++ as-config-default.php
@@ -0,0 +1,22 @@
+<?php 
+
+// Authentication System config file 
+
+// Database Settings here
+
+// Database Name 
+define('DB_NAME', '');
+
+// Database User
+define('DB_USER', '');
+
+// Database User password
+define('DB_PASS', '');
+
+// Database host
+define('DB_HOST', '');
+
+// Database charset
+define('DB_CHARSET', '');
+
+?>
